8321b6460c0514a9af212ae2c55a10fce8eeb16f
sagar kumar <94098920+Sagar-Kumar01@users.noreply.github.com>
2025-11-20 15:46:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your code here
+const url = "https://randomuser.me/api/";
+
+let img = document.getElementById("img");
+let info = document.getElementById("info");
+let abtn = document.getElementById("abtn");
+let ebtn = document.getElementById("ebtn");
+let pbtn = document.getElementById("pbtn");
+let getUser = document.getElementById("getUser");
+let name = document.getElementById("name");
+
+let totaldata = null;
+
+async function getData() {
+  let res = await fetch(url);
+  let data = await res.json();
+  totaldata = data.results[0];
+}
+
+getUser.addEventListener("click", async () => {
+	await getData();
+  
+    img.setAttribute("src", totaldata.picture.large);
+	name.textContent = `${totaldata.name.first} ${totaldata.name.last}`
+});
+abtn.addEventListener("click", () => {
+	if(totaldata != null){
+	info.style.display = "initial"
+	info.textContent = `${totaldata.dob.age}`
+	}
+});
+ebtn.addEventListener("click", () => {
+	if(totaldata != null){
+	info.style.display = "initial"
+	info.textContent = `${totaldata.email}`
+	}
+});
+pbtn.addEventListener("click", () => {
+	if(totaldata != null){
+	info.style.display = "initial"
+	info.textContent = `${totaldata.phone}`
+	}
+});
+
+
+
+
+
+
